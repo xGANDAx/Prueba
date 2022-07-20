cd857d712f84bc12ec6fb5f4e27f24ea78604455
Jonathan Chitiva <jonathanchitivam@gmail.com>
2022-07-20 03:14:38 -0500

    Realizacion de la prueba

--- test/main.py
+++ test/main.py
@@ -3,10 +3,21 @@
 MAIN
 """
 import json
+from multiprocessing.sharedctypes import Value
+from tkinter import W
+from turtle import back
+from colorama import init, Back
 import requests
+
+init()
 
 def run():
     r = requests.get('https://api.chucknorris.io/jokes/random')
     with open('json-joker.json', 'w') as dataFile:
         json.dump(r.json(), dataFile, indent=4)
+    with open('json-joker.json') as dataFile:
+        data = json.load(dataFile)
+        print(Back.BLUE,data)     
+        with open('joker.txt','w') as textFile:
+            textFile.write(data["value"])
     return 1
